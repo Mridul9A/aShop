ef2ff5ccd5e52054d99d04a0520912ed2dec9521
Mridul9A <mridul9a@gmail.com>
2025-03-22 12:12:33 +0530

    Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <h1>Home Page</h1>
+      <p>This is home page. It is for showing all products and every highlights.</p>
+    </div>
   )
 }
 
